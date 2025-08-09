a4359176b16be7d4f43e057f2b49a2dc2e7de49e
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-08-09 14:05:29 -0600

    feat: create script to run cloud functions locally

---
+++ main.py
@@ -0,0 +1,15 @@
+import importlib
+import sys
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+    location = Path(__file__).parent / "functions"
+    sys.path.insert(0, str(location))
+
+    func, *args = sys.argv[1:]
+    mod = importlib.import_module(name=f"{func}.main")
+    run = getattr(mod, func)
+    run(*args)
