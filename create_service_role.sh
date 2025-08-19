24acd43ac26d95e5bc0900015c75bf14c9f44a56
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-08-18 19:54:12 -0600

    style: add service_account variable to make script cleaner and add usage comment

--- create_service_role.sh
+++ create_service_role.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-
+# Usage: ./create_service_role.sh <project_id> # NOTE: project id NOT project number
+set -euo pipefail
 PROJECT_ID="${1}"
+SERVICE_ACCOUNT="terraform-admin@${PROJECT_ID}.iam.gserviceaccount.com"
 
 gcloud auth application-default login
-
 gcloud iam service-accounts create terraform-admin \
     --description="Terraform admin service account" \
-    --display-name="Terraform Admin" --project=${PROJECT_ID}
-
-
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:terraform-admin@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role="roles/editor"
-
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:terraform-admin@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role="roles/iam.serviceAccountAdmin"
-
-# Create and download key
-gcloud iam service-accounts keys create terraform-key.json \
-    --iam-account=terraform-admin@${PROJECT_ID}.iam.gserviceaccount.com \
+    --display-name="Terraform Admin" \
     --project=${PROJECT_ID}
 
-mv terraform-key.json .terraform/terraform-key.json
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member="serviceAccount:${SERVICE_ACCOUNT}" \
+    --role="roles/editor"
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member="serviceAccount:${SERVICE_ACCOUNT}" \
+    --role="roles/iam.serviceAccountAdmin"
+
+gcloud iam service-accounts keys create .terraform/terraform-key.json \
+    --iam-account=${SERVICE_ACCOUNT} \
+    --project=${PROJECT_ID}
