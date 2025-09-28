20143f81abdbf6589eb7a35739b00b4bd5563722
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-09-28 01:13:52 -0600

    chore: use same local_file provisioner for both reconnect scripts depending on environment

--- terraform/modules/chrome_vm/main.tf
+++ terraform/modules/chrome_vm/main.tf
@@ -31,46 +31,36 @@ resource "google_compute_instance" "chrome_vm" {
   metadata = {
     ssh-keys = "${var.ssh_user}:${file(var.ssh_public_key_file)}"
     startup-script = templatefile("${path.module}/setup-chrome-vm.sh", {
-      upload_function_url = var.upload_function_url
-      api_key = var.api_key
-      SSH_USER = var.ssh_user
+      upload_function_url   = var.upload_function_url
+      api_key               = var.api_key
+      SSH_USER              = var.ssh_user
       extension_remote_path = var.extension_remote_path
-      period = var.period
+      period                = var.period
     })
   }
 
   tags = ["chrome-vm"]
 }
 
-resource "local_file" "reconnect_script_bash" {
-  content = templatefile("${path.module}/reconnect.sh.tpl", {
-    SSH_USER = var.ssh_user
-    VM_IP    = google_compute_instance.chrome_vm.network_interface[0].access_config[0].nat_ip
+resource "local_file" "reconnect_script" {
+  content = templatefile("${path.module}/reconnect.${var.windows ? "ps1" : "sh"}.tpl", {
+    SSH_USER             = var.ssh_user
+    VM_IP                = google_compute_instance.chrome_vm.network_interface[0].access_config[0].nat_ip
     SSH_PRIVATE_KEY_FILE = var.ssh_private_key_file
   })
-  filename = "${path.root}/reconnect.sh"
-  file_permission = "0755"
-}
-
-resource "local_file" "reconnect_script_powershell" {
-  content = templatefile("${path.module}/reconnect.ps1.tpl", {
-    SSH_USER = var.ssh_user
-    VM_IP    = google_compute_instance.chrome_vm.network_interface[0].access_config[0].nat_ip
-    SSH_PRIVATE_KEY_FILE = var.ssh_private_key_file
-  })
-  filename = "${path.root}/reconnect.ps1"
+  filename        = "${path.root}/reconnect.${var.windows ? "ps1" : "sh"}"
   file_permission = "0755"
 }
 
 resource "terraform_data" "cleanup_host_key" {
   input = {
-    vm_ip = google_compute_instance.chrome_vm.network_interface[0].access_config[0].nat_ip
+    vm_ip   = google_compute_instance.chrome_vm.network_interface[0].access_config[0].nat_ip
     windows = var.windows
   }
 
   provisioner "local-exec" {
-    when    = destroy
-    command = self.input.windows ? "ssh-keygen -f \"%USERPROFILE%\\.ssh\\known_hosts\" -R \"${self.input.vm_ip}\" 2>nul || echo done" : "ssh-keygen -f ~/.ssh/known_hosts -R '${self.input.vm_ip}' 2>/dev/null || true"
+    when        = destroy
+    command     = self.input.windows ? "ssh-keygen -f \"%USERPROFILE%\\.ssh\\known_hosts\" -R \"${self.input.vm_ip}\" 2>nul || echo done" : "ssh-keygen -f ~/.ssh/known_hosts -R '${self.input.vm_ip}' 2>/dev/null || true"
     interpreter = self.input.windows ? ["cmd", "/c"] : ["bash", "-c"]
   }
 }
